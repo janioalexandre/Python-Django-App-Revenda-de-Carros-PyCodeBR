5fb189401c0f9e5fdd618347875c05d8f5fa3172
Janio Alexandre <janioalexandre@gmail.com>
2024-11-06 15:47:54 -0300

    ORM e Querys no Django

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from cars.models import Car
 
 
 def cars_view(request):
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+    
+    if search:
+        cars = Car.objects.filter(model__contains=search)  
+
     return render(
         request, 
         'cars.html',
-        {'cars': {'model': 'Astra 20'}}
+        {'cars': cars}
     )
