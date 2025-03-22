720bbb3c1561672e6c854f86b17acf509c919148
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-03-21 23:48:09 -0300

    ORM e Querys no Django - Fazendo consultas com filter

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.filter(model__contains='Prisma')
 
     return render(
         request, 
