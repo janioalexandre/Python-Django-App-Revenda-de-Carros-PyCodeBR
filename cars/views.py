c93afea82df4b17b8f90efa1d614227f01ca10b7
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-03-23 16:55:06 -0300

    Criando template base do sistema

--- cars/views.py
+++ cars/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
     search = request.GET.get('search')
 
     if search:
-        cars = Car.objects.filter(model__contains=search)
-
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
+    
     return render(
         request, 
         'cars.html',
