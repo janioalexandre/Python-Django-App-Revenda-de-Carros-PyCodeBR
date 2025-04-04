5b5ebed229f3dffd4d4cb418768147eb57a62a24
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-04-03 23:53:12 -0300

    Utilizando DetailView

--- cars/views.py
+++ cars/views.py
@@ -17,21 +17,13 @@ class CarsListView(ListView):
             cars = cars.filter(model__icontains=search)
         return cars
 
-class NewCarView(View):
-    
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
-
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
-
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+    context_object_name = 'car'
