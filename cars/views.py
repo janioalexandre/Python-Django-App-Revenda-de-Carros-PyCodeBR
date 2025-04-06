5e7193191299bf956fcda0ac3642da48922d0fb4
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-04-06 17:36:24 -0300

    Utilizando DeleteView

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 class CarsListView(ListView):
@@ -32,4 +31,11 @@ class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
     template_name = 'car_update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
     success_url = '/cars/'
