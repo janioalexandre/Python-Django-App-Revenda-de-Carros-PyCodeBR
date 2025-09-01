f47b49887be2be53c21a148912465077f750cfd4
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-09-01 12:01:55 -0300

    Configure environment variables for OpenAI API key

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# OpenAI API Key
+OPENAI_API_KEY = config('OPENAI_API_KEY')
--- openai_api/client.py
+++ openai_api/client.py
@@ -1,17 +1,18 @@
 from openai import OpenAI
+from django.conf import settings
 
 
 client = OpenAI(
-    api_key='API_KEY'
+    api_key=settings.OPENAI_API_KEY
 )
 
 
 def get_car_ai_bio(model, brand, year):
-    message = ''''
+    message = '''
     Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. Fale coisas específicas desse modelo.
     Descreva especificações técnicas desse modelo de carro.
     '''
-    message = message.format(brand, model, year)
+    message = message.format(brand, model, year).strip()
     response = client.chat.completions.create(
         messages=[
             {
@@ -23,4 +24,4 @@ def get_car_ai_bio(model, brand, year):
         model='gpt-3.5-turbo',
     )
 
-    return response.choices[0].message.content
+    return response.choices[0].message.content.strip()
